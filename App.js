a58cca62724e03b6c31a78b360dce2b13fb30e6e
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-09 16:32:09 -0500

    wire redux store

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'; 
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>let's build uber</Text>
         {/* <StatusBar style="auto" /> */}
