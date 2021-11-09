e51e7253ee1be43ef3e213c8ff9a588ef736ce6b
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-09 16:13:10 -0500

    create navSlice function

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -4,4 +4,21 @@ const initialState = {
     origin: null,
     destination: null, 
     travelTimeInformation: null
-}
+};
+
+export const navSlice = createSlice({
+    name: 'nav', 
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        settravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
+    }
+
+});
