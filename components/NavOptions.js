2d715286a57023be7a8aeacc2de7f008123aeb20
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-10 13:47:14 -0500

    create data obj

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,10 @@
 import React from 'react'; 
-import { StyleSheet, View, Text } from 'react-native'; 
+import { StyleSheet, View, Text } from 'react-native';  
+
+const data = [
+    {id: '123', title: 'Get a ride', image:"https://links.papareact.com/3pn", screen: 'MapScreen',},
+    {id: '456', title: 'Order food', image:"https://links.papareact.com/28w", screen: 'EatScreen',},
+]
 
 const NavOptions = () => {
     return(
