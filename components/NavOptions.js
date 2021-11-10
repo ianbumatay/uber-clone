128f9343b4b600335b1555593d6cdfbc4cd6d911
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-10 13:30:24 -0500

    create NavOptions function

---
+++ components/NavOptions.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import { StyleSheet, View, Text } from 'react-native'; 
+
+const NavOptions = () => {
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    );
+
+}; 
+
+export default NavOptions 
+
+const styles = StyleSheet.create({})
