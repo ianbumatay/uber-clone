c0e61f211fe03b1a63b509f3fc4b5ec5b2b500ab
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-10 13:21:00 -0500

    add image

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, Image} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 const HomeScreen = () => {
     return(
         <SafeAreaView style={tw`bg-white h-full`}>
-            <Text> Home Screen </Text>
+          <View>
+              <Image
+                style={{width: 100,  height:  100, resizeMode: 'contain'}}
+                source={{
+                    uri: "https://links.papareact.com/gzs"
+                }}
+              />
+          </View>
         </SafeAreaView>
     );
 }; 
