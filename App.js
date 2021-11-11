59cbbd75b6792d55bc3497f2b3d1396823de23b0
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-11 10:06:00 -0500

    import and implement SaferAreaProvider component

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'; 
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <HomeScreen/>
-      </View>
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <HomeScreen/>
+        </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
