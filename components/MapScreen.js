f89558b7140ab3bc616582d86474fffc5c8bb210
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-11 13:11:30 -0500

    create MapScreen function

---
+++ components/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import {StyleSheet, View, Text} from 'react-native'; 
+
+const MapSreen = () => {
+    return(
+        <View>
+            <Text>
+                Map Screen
+            </Text>
+        </View>
+    );
+}; 
+
+export default MapSreen
