394a3b66862702ad13e5fe01b3155834376f52da
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-11 14:44:48 -0500

    implement Mapscreen navigation

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
              <Stack.Screen
               name="MapScreen"
               component={MapScreen} 
-              options={{headerShown: false}}
+              // options={{headerShown: false}}
             />
           </Stack.Navigator>
         </SafeAreaProvider>
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
-import tw from 'tailwind-react-native-classnames'; 
-import {Icon} from 'react-native-elements'
+import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/core';
 
 const data = [
     { id: '123', title: 'Get a ride', image: "https://links.papareact.com/3pn", screen: 'MapScreen', },
     { id: '456', title: 'Order food', image: "https://links.papareact.com/28w", screen: 'EatScreen', },
 ]
 
-const NavOptions = () => {
+const NavOptions = () => { 
+
+    const navigation = useNavigation();
+    
+
     return (
         <View>
             <FlatList
@@ -16,14 +21,24 @@ const NavOptions = () => {
                 data={data}
                 horizontal
                 renderItem={({ item }) => (
-                    <TouchableOpacity style={tw`p-2 pl-6 pt-4 pb-8 bg-gray-200 m-2 w-40`}>
+                    <TouchableOpacity 
+                    style={tw`p-2 pl-6 pt-4 pb-8 bg-gray-200 m-2 w-40`}  
+                    onPress={()=> navigation.navigate(item.screen)}
+                    > 
+
                         <View>
                             <Image
                                 style={{ width: 120, height: 120, resizeMode: 'contain' }}
                                 source={{ uri: item.image }}
-                            /> 
-                            <Text style={tw`text-lg mt-2 font-semibold`}>{item.title}</Text> 
-                            <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name="arrowright" color="white" type="antdesign"/>
+                            />
+                            <Text
+                                style={tw`text-lg mt-2 font-semibold`}>
+                                {item.title}
+                            </Text>
+                            <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                                name="arrowright"
+                                color="white"
+                                type="antdesign" />
                         </View>
                     </TouchableOpacity>
                 )}
@@ -37,4 +52,3 @@ export default NavOptions
 
 const styles = StyleSheet.create({})
 
-            
