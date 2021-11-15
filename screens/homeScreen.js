0c91fe2925aa39d18a83153fe99c90b824f17249
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-15 14:15:15 -0500

    set up Googlrplaces autocomplete component

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView, Image} from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
-import NavOptions from '../components/NavOptions'; 
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'; 
-import {GOOGLE_MAPS_APIKEY} from "@env";
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 
 const HomeScreen = () => {
-    return(
-        <SafeAreaView style={tw`bg-white h-full`}>
-          <View style={tw`p-5`}>
-              <Image
-                style={{width: 100,  height:  100, resizeMode: 'contain'}}
-                source={{
-                    uri: "https://links.papareact.com/gzs"
-                }}
-              /> 
-              <NavOptions/>
-          </View>
-        </SafeAreaView>
-    );
-}; 
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          source={{
+            uri: "https://links.papareact.com/gzs"
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="where from?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen 
+export default HomeScreen
 
 const styles = StyleSheet.create({});
