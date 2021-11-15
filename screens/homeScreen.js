18a2b56a9f1bee5a46f248b487422a1e95088c96
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-15 14:01:27 -0500

    import google_maps_apiikey

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Text, SafeAreaView, Image} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions'; 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'; 
+import {GOOGLE_MAPS_APIKEY} from "@env";
 
 
 const HomeScreen = () => {
