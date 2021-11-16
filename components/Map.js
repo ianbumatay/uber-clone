fa4d234f676b464a8694f10b942e8c909ce4b9bb
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-16 13:45:21 -0500

    create Map component

---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import {StyleSheet, View, Text} from 'react-native'; 
+
+const Map = () => {
+    return(
+        <View>
+            <Text>this is a map</Text>
+        </View>
+    )
+} 
+
+export default Map 
+
+const styles = StyleSheet.create({})
