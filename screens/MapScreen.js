74ab97d4064eefb38dc5f3dc17d48de27d000b99
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-16 13:39:00 -0500

    create component to hose the map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,15 @@
 import React from 'react'; 
 import {StyleSheet, View, Text} from 'react-native'; 
+import tw from 'tailwind-react-native-classnames';
 
 const MapSreen = () => {
     return(
         <View>
             <Text>
                 Map Screen
-            </Text>
+            </Text> 
+            <View style={tw`h-1/2`}></View>
+            <View style={tw`h-1/2`}></View>
         </View>
     );
 }; 
