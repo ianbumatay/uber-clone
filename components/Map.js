181e8f1710b2cf24951097196d806443a69c99da
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-11-17 10:30:42 -0500

    import useSelector hook

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
 import { selectOrigin} from '../slices/navSlice';
 
@@ -15,8 +16,8 @@ const Map = () => {
                 style={tw`flex-1`}
                 mapType="mutedStandard"
                 initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
+                    latitude: origin.location.lat,
+                    longitude: origin.location.lng,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
